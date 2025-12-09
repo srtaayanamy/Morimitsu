0b430d469dd8976b5bf0e30d7a0ec403bf35f82f
srtaayanamy <annafelix448@gmail.com>
2025-12-09 13:53:44 -0300

    feat: máscara CPF

--- src/pages/RegistrarAluno.tsx
+++ src/pages/RegistrarAluno.tsx
@@ -70,6 +70,22 @@ export default function RegistrarAluno() {
     setTurmaSelecionada("");
   }
 
+  const formatarCPF = (value: string) => {
+  value = value.replace(/\D/g, "");
+  if (value.length > 11) value = value.slice(0, 11);
+
+  if (value.length <= 3) return value;
+  if (value.length <= 6) return `${value.slice(0, 3)}.${value.slice(3)}`;
+  if (value.length <= 9)
+    return `${value.slice(0, 3)}.${value.slice(3, 6)}.${value.slice(6)}`;
+
+  return `${value.slice(0, 3)}.${value.slice(3, 6)}.${value.slice(
+    6,
+    9
+  )}-${value.slice(9)}`;
+};
+
+
   const formatarTelefone = (value: string) => {
     value = value.replace(/\D/g, "");
     if (value.length > 11) value = value.slice(0, 11);
@@ -242,7 +258,7 @@ export default function RegistrarAluno() {
               <input
                 type="text"
                 className={inputBase}
-                value={CPF}
+                value={formatarCPF(CPF)}
                 onChange={handleCPFChange}
               />
             </div>
