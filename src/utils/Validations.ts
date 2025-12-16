521fae231aaaec4abff00b85a15f76ae78faf38f
josineide souza <isaquesouzasilveira@gmail.com>
2025-12-16 16:06:25 -0300

    fix: inserção de uma validação para data.

--- src/utils/Validations.ts
+++ src/utils/Validations.ts
@@ -9,3 +9,14 @@ export function validateCPF(CPF: string): boolean{
   const CPFRegex = /^\d{11}$/;
   return CPFRegex.test(CPF);
 }
+
+export function isDateValid(date: Date | string): boolean {
+  const inputDate = new Date(date);
+  const now = new Date();
+
+  // Remove horas/minutos/segundos se quiser comparar só a data
+  inputDate.setHours(0, 0, 0, 0);
+  now.setHours(0, 0, 0, 0);
+
+  return inputDate >= now;
+}
