c624049ffe8a3b95c80fd465db24e2cf5bf4f227
srtaayanamy <annafelix448@gmail.com>
2025-12-16 23:03:48 -0300

    tooltip de grau

--- src/components/GraduandosSection.tsx
+++ src/components/GraduandosSection.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BeltTag from "./BeltTag";
-import { Avatar } from "./Avatar";
 import SectionCard from "./SectionCard";
 
 interface Graduando {
